da73c868e025f60070fa79be22eba14adc90475b
AndreMonica <andregodinhomonica@gmail.com>
2023-04-24 07:29:16 +0100

    ?

---
+++ PHP/connect_db.php
@@ -0,0 +1,17 @@
+<?php
+	$_conn;
+
+	$servername = $_POST['servername'];
+	$username = $_POST['username'];
+	$password = $_POST['password'];
+	$dbname = $_POST['dbname'];
+
+	$_conn = new mysqli($servername, $username, $password, $dbname);
+	if ($conn->connect_error) {
+		die("Connection failed: " . $conn->connect_error);
+	}
+	$_SESSION["conn"] = $conn;
+	// Redirect to the viewpage
+	header('Location: http://localhost/SqlBPweb/HTML/view.html');
+
+?>
